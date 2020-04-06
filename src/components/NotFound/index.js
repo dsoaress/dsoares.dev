cd927baa90f462e584a31c9837b59aaaaa6df440
Daniel Soares <daniel.soares@me.com>
2020-04-06 05:48:33 -0300

    :sparkles: Adding NotFound component

---
+++ src/components/NotFound/index.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import * as S from "./styled"
+
+const NotFound = () => (
+  <S.NotFoundWrapper>
+    <S.Icon />
+    <h1>NÃO ENCONTRADO</h1>
+    <p>Você acabou de pegar uma rota que não existe... </p>
+  </S.NotFoundWrapper>
+)
+
+export default NotFound
---
+++ src/components/NotFound/styled.js
@@ -0,0 +1,14 @@
+import styled from "styled-components"
+
+import { MessageSquareError } from "@styled-icons/boxicons-solid/MessageSquareError"
+
+export const NotFoundWrapper = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`
+
+export const Icon = styled(MessageSquareError)`
+  width: 60px;
+  color: #bc2d57;
+`
