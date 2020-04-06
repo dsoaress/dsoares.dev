4b11f92297183283a6b79b07bd0eea544f9da669
Daniel Soares <daniel.soares@me.com>
2020-04-06 05:53:30 -0300

    :sparkles: Creating some pages

---
+++ src/pages/blog.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const BlogPage = () => (
+  <Layout>
+    <SEO title="Blog" />
+    <h1>Blog</h1>
+  </Layout>
+)
+
+export default BlogPage
