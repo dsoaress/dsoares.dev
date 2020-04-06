66a5694417bcd0fb1c44dbb0453348130146faa5
Daniel Soares <daniel.soares@me.com>
2020-04-06 05:48:00 -0300

    :sparkles: Adding Footer component

---
+++ src/components/Footer/index.js
@@ -0,0 +1,14 @@
+import React from "react"
+
+import * as S from "./styled"
+
+const Footer = () => (
+  <S.Footer>
+    <S.FooterItens>
+      Made with <S.HeartIcon /> and <a href="https://gatsbyjs.org">Gatsby</a>
+    </S.FooterItens>
+    <S.FooterItens>Daniel Soares</S.FooterItens>
+  </S.Footer>
+)
+
+export default Footer
