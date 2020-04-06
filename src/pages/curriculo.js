00eb83949df03d5ef03f4c1966251433780e3c93
Daniel Soares <daniel.soares@me.com>
2020-04-06 05:53:30 -0300

    :sparkles: Creating some pages

---
+++ src/pages/curriculo.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+const CVPage = () => (
+  <Layout>
+    <SEO title="Currículo" />
+    <h1>Currículo</h1>
+  </Layout>
+)
+
+export default CVPage
