9ed6059d0e69135c3e242ca5c0bd4914b79eb2b8
Daniel Soares <daniel.soares@me.com>
2020-04-06 05:48:00 -0300

    :sparkles: Adding Footer component

---
+++ src/components/Footer/styled.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+import { Heart } from "@styled-icons/typicons/Heart"
+
+export const Footer = styled.footer`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 80px;
+  font-size: 12px;
+`
+
+export const FooterItens = styled.p`
+  margin: -2px;
+`
+
+export const HeartIcon = styled(Heart)`
+  width: 20px;
+  height: 20px;
+  margin-bottom: 5px;
+  fill: #bc2d57;
+`
