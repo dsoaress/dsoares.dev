0035f9a8e90c975051c2afbebe626304d0c6fe97
Daniel Soares <daniel.soares@me.com>
2020-04-06 05:50:53 -0300

    :sparkles: Updating NotFound page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,12 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import NotFound from "../components/NotFound"
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <NotFound />
   </Layout>
 )
 
