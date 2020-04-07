7d19e547a8c055e6c14da840eae63e14cc658268
Daniel Soares <daniel.soares@me.com>
2020-04-07 02:56:35 -0300

    🚧 Building CV page

--- src/pages/curriculo.js
+++ src/pages/curriculo.js
@@ -2,11 +2,12 @@ import React from "react"
 
 import Layout from "../components/Layout"
 import SEO from "../components/SEO"
+import CV from "../components/CV"
 
 const CVPage = () => (
   <Layout>
     <SEO title="Currículo" />
-    <h1>Currículo</h1>
+    <CV />
   </Layout>
 )
 
