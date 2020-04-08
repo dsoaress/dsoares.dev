7315f95a71ed57dd6ff2009ee4a1656623b08a6f
Daniel Soares <daniel.soares@me.com>
2020-04-08 17:29:17 -0300

    💄 Changing the default theme

--- src/html.js
+++ src/html.js
@@ -13,7 +13,7 @@ export default function HTML(props) {
         />
         {props.headComponents}
       </head>
-      <body {...props.bodyAttributes} className="dark">
+      <body {...props.bodyAttributes} className="light">
         <script
           dangerouslySetInnerHTML={{
             __html: `
@@ -35,7 +35,7 @@ export default function HTML(props) {
                   localStorage.setItem('theme', newTheme);
                 } catch (err) {}
               }
-              setTheme(preferredTheme || 'dark');
+              setTheme(preferredTheme || 'light');
             })();
           `,
           }}
