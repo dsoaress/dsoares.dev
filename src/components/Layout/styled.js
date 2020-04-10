f97bc45d7cf9b01a81a21c439da17cf6e67c2251
Daniel Soares <daniel.soares@me.com>
2020-04-10 03:02:42 -0300

    💄 Fixing alignment

--- src/components/Layout/styled.js
+++ src/components/Layout/styled.js
@@ -4,7 +4,6 @@ import media from "styled-media-query"
 export const LayoutWrapper = styled.section`
   display: flex;
   flex-direction: column;
-  justify-content: space-between;
   min-height: calc(var(--vh, 1vh) * 100);
 `
 
@@ -12,6 +11,8 @@ export const MainWrapper = styled.main`
   max-width: 1024px;
   margin: 0 auto;
   padding: 0 3%;
+  display: flex;
+  flex: 1;
 
   ${media.lessThan("600px")`
     padding: 0 20px;
