fa3b3a9f4a15447062628ba0dfebebb35a307bfb
Daniel Soares <daniel.soares@me.com>
2020-04-10 04:17:07 -0300

    🥚 Adding a easter egg

---
+++ src/pages/wp-admin.js
@@ -0,0 +1,15 @@
+import React from "react"
+import styled from "styled-components"
+
+import gif from "../images/wp-admin.gif"
+
+export const WPAdminWrapper = styled.section`
+  background: url(${gif}) center center no-repeat;
+  background-size: cover;
+  width: 100%;
+  min-height: calc(var(--vh, 1vh) * 100);
+`
+
+const WPAdminPage = () => <WPAdminWrapper />
+
+export default WPAdminPage
