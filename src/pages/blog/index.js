8bf80bce92aa22fd480f3808f806f4fb368c1a6c
Daniel Soares <daniel.soares@me.com>
2020-04-11 06:35:35 -0300

    🚚 Fixing posts path

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -1,8 +1,8 @@
 import React from "react"
 
-import Layout from "../components/Layout"
-import SEO from "../components/SEO"
-import BlogList from "../components/BlogList"
+import Layout from "../../components/Layout"
+import SEO from "../../components/SEO"
+import BlogList from "../../components/BlogList"
 
 const BlogPage = () => (
   <Layout>
