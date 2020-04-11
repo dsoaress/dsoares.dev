f0c75c02c9767541b26aa7e663a944b463c17942
Daniel Soares <daniel.soares@me.com>
2020-04-11 10:27:39 -0300

    💬 Updating title on NotFound page

--- src/pages/404.js
+++ src/pages/404.js
@@ -6,7 +6,7 @@ import NotFound from "../components/NotFound"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
+    <SEO title="Não encontrado" />
     <NotFound />
   </Layout>
 )
