068fa904f8dd1f2e0208eaffd0a09bffed29554f
Daniel Soares <daniel.soares@me.com>
2020-04-14 09:38:01 -0300

    ✨ Adding "sobre mim" page and removing CV

---
+++ src/components/About/styled.js
@@ -0,0 +1,49 @@
+import styled from "styled-components"
+import media from "styled-media-query"
+
+import Img from "gatsby-image"
+
+export const AboutWrapper = styled.div`
+  max-width: 800px;
+`
+
+export const AboutTitle = styled.h1``
+
+export const AboutFlex = styled.div`
+  display: flex;
+
+  ${media.lessThan("600px")`
+    flex-direction: column;
+  `}
+`
+
+export const AboutDesc = styled.div`
+  width: 65%;
+  padding-bottom: 30px;
+
+  ${media.lessThan("780px")`
+  width: 55%;
+  `}
+
+  ${media.lessThan("600px")`
+    width: 100%;
+  `}
+`
+
+export const AboutDescP = styled.p``
+
+export const AboutImage = styled(Img)`
+  width: 30%;
+  height: 450px;
+  border-radius: 3px;
+  margin-left: 5%;
+
+  ${media.lessThan("780px")`
+  width: 40%;
+  `}
+
+  ${media.lessThan("600px")`
+  width: 100%;
+  margin-left: 0;
+  `}
+`
---
+++ src/templates/aboutPage.js
@@ -0,0 +1,35 @@
+import React from "react"
+import { graphql } from "gatsby"
+
+import Layout from "../components/Layout"
+import SEO from "../components/SEO"
+
+import About from "../components/About"
+
+const AboutPage = ({ data }) => {
+  return (
+    <Layout>
+      <SEO title={data.markdownRemark.frontmatter.title} />
+      <About
+        title={data.markdownRemark.frontmatter.title}
+        text={
+          <div dangerouslySetInnerHTML={{ __html: data.markdownRemark.html }} />
+        }
+      />
+    </Layout>
+  )
+}
+
+export const query = graphql`
+  query AboutPage($id: String!) {
+    markdownRemark(id: { eq: $id }) {
+      frontmatter {
+        title
+      }
+      id
+      html
+    }
+  }
+`
+
+export default AboutPage
