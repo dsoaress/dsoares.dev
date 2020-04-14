dd0d35582470f4e0b47d2df518232eeb03672600
Daniel Soares <daniel.soares@me.com>
2020-04-14 09:23:41 -0300

    💄 Adding some links and improving the layout

--- src/components/SocialLinks/content.js
+++ src/components/SocialLinks/content.js
@@ -11,6 +11,14 @@ const links = [
     label: "Github",
     url: `https://github.com/dsoaress`,
   },
+  {
+    label: "Whatsapp",
+    url: `https://api.whatsapp.com/send?phone=5545988031679`,
+  },
+  {
+    label: "Email",
+    url: `mailto:daniel.soares@me.com`,
+  },
 ]
 
 export default links
