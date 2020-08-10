3d5b0c5acafbaf4395826107f28d5f1b7dd2ea60
Daniel Soares <daniel.soares@me.com>
2020-08-09 23:02:31 -0300

    :sparkles: add Header, Footer and GlobalStyles to Layout

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 
+import Header from '../Header'
+import Footer from '../Footer'
+import GlobalStyles from './global'
+import * as S from './styled'
+
 const Layout = ({ children }) => (
-  <>
+  <S.LayoutWrapper>
+    <GlobalStyles />
+    <Header />
     <main>{children}</main>
-  </>
+    <Footer />
+  </S.LayoutWrapper>
 )
 
 Layout.propTypes = {
---
+++ src/components/Layout/styled.js
@@ -0,0 +1,7 @@
+import styled from 'styled-components'
+
+export const LayoutWrapper = styled.div`
+  display: grid;
+  grid-template-rows: 140px 1fr 140px;
+  min-height: calc(var(--vh, 1vh) * 100);
+`
