d4881681b76a7f1c517dae695fb6e0abb5913dec
Daniel Soares <daniel.soares@me.com>
2020-08-11 09:28:49 -0300

    :lipstick: style 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,28 @@
 import React from 'react'
+import styled from 'styled-components'
 
 import Layout from '../components/Layout'
 import SEO from '../components/SEO'
 
+const NotFoundWrapper = styled.div`
+  display: grid;
+  align-content: center;
+  height: 100%;
+  text-align: center;
+`
+
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>Não encontrado</h1>
-    <p>O que você está buscando não existe.</p>
+    <SEO title="404: não encontrado" />
+    <NotFoundWrapper>
+      <h1>
+        Não encontrado{' '}
+        <span role="img" aria-label="sed">
+          😥
+        </span>
+      </h1>
+      <p>O que você está buscando não existe.</p>
+    </NotFoundWrapper>
   </Layout>
 )
 
