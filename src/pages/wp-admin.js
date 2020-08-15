4fd458690376f2c0716763fb13170e8f95ece291
Daniel Soares <daniel.soares@me.com>
2020-08-15 12:10:21 -0300

    :egg: add a easter egg

---
+++ src/pages/wp-admin.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import styled from 'styled-components'
+
+import video from '../../content/assets/wp-admin.mp4'
+
+export const WPAdminWrapper = styled.video`
+  min-width: 100%;
+  min-height: calc(var(--vh, 1vh) * 100);
+  position: fixed;
+  right: 0;
+  bottom: 0;
+`
+
+const WPAdminPage = () => (
+  <WPAdminWrapper autoplay loop>
+    <source src={video} type="video/mp4" />
+  </WPAdminWrapper>
+)
+
+export default WPAdminPage
