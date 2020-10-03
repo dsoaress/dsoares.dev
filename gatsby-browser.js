d1d8040273329155a6084d3c75346fbda34f19c4
Daniel Soares <daniel.soares@me.com>
2020-10-02 21:11:47 -0300

    💥🚧 Change Gatsby by Next

--- gatsby-browser.js
+++
@@ -1,9 +0,0 @@
-// Viewport height correction on mobile devices
-
-let vh = window.innerHeight * 0.01
-document.documentElement.style.setProperty('--vh', `${vh}px`)
-
-window.addEventListener('resize', () => {
-  let vh = window.innerHeight * 0.01
-  document.documentElement.style.setProperty('--vh', `${vh}px`)
-})
