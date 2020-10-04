0a50106389f8378f2b3246f231e96aec8f4c8b3c
Daniel Soares <daniel.soares@me.com>
2020-10-04 16:10:48 -0300

    ✨ Add Strapi

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '544ac4f7fa399f6f45736d176223020b')
+    }
+  }
+})
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '584c154c-b24d-4a93-8e31-5b03b3dd9743'
+}
