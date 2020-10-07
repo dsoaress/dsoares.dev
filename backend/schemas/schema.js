34315051daa44060f14b00e1c3083655bab91e81
Daniel Soares <daniel.soares@me.com>
2020-10-07 10:18:34 -0300

    🚧 Add siteSettings

--- backend/schemas/schema.js
+++ backend/schemas/schema.js
@@ -8,7 +8,7 @@ import schemaTypes from 'all:part:@sanity/base/schema-type'
 import blockContent from './blockContent'
 import category from './category'
 import post from './post'
-import profile from './profile'
+import siteSettings from './siteSettings'
 import youtube from './youtube'
 
 // Then we give our schema to the builder and provide the result to Sanity
@@ -22,7 +22,7 @@ export default createSchema({
     // in the studio.
     post,
     category,
-    profile,
+    siteSettings,
     // When added to this list, object types can be used as
     // { type: 'typename' } in other document schemas
     blockContent,
--- backend/schemas/siteSettings.js
+++ backend/schemas/siteSettings.js
@@ -1,7 +1,7 @@
 export default {
-  name: 'profile',
-  title: 'Perfil',
+  name: 'siteSettings',
   type: 'document',
+  __experimental_actions: [/*'create',*/ 'update', /*'delete',*/ 'publish'],
   fields: [
     {
       name: 'name',
