2203660be79eb804960969be91bb267a47d5a4a5
Daniel Soares <daniel.soares@me.com>
2020-10-11 11:59:14 -0300

    🚧 Working on header

--- src/components/header.js
+++ src/components/header.js
@@ -1,3 +1,6 @@
+/** @jsx jsx */
+import { jsx } from 'theme-ui'
+import { useStaticQuery, graphql } from 'gatsby'
 import Container from '@components/container'
 import Link from '@components/link'
 
@@ -16,16 +19,40 @@ const Header = () => {
     .filter(e => e.trim().length > 0)
 
   return (
-    <header>
-      <Link to="/" alt="Home">
-        {title[0]}
-        <span>{title[1]}</span>
-      </Link>
-      <nav>
-        <Link to="/sobre-mim" alt="Sobre mim" activeClassName="active">
+    <header
+      sx={{
+        variant: 'styles.header',
+        borderTop: '10px solid',
+        borderColor: 'gray'
+      }}
+    >
+      <Container>
+        <Link
+          to="/"
+          alt="Home"
+          sx={{
+            variant: 'styles.navlink',
+            fontSize: 5,
+            py: 2
+          }}
+        >
+          {title[0]}
+          <span>{title[1]}</span>
+        </Link>
+
+        <Link
+          to="/sobre-mim"
+          alt="Sobre mim"
+          activeClassName="active"
+          sx={{
+            variant: 'styles.navlink',
+            ml: 3,
+            py: 2
+          }}
+        >
           Sobre mim
         </Link>
-      </nav>
+      </Container>
     </header>
   )
 }
