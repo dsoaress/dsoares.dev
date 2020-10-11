57239a3a08065370f778c081134b33eba5b42e53
Daniel Soares <daniel.soares@me.com>
2020-10-11 11:58:05 -0300

    💄 Add components

---
+++ src/components/container.js
@@ -0,0 +1,6 @@
+/** @jsx jsx */
+import { jsx, Container as ContainerThemeUI } from 'theme-ui'
+
+const Container = props => <ContainerThemeUI {...props} sx={{ p: 4 }} />
+
+export default Container
