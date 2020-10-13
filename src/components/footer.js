afa5e7ff48c3e6838bcd97fa60436840e329d4b6
Daniel Soares <daniel.soares@me.com>
2020-10-13 11:12:32 -0300

    💄 Switch footer to tw

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,25 +1,22 @@
 import React from 'react'
-import styled from 'styled-components'
-import { color, grid, layout, space, typography } from 'styled-system'
-
-import Container from '@components/container'
 import { AiTwotoneHeart } from 'react-icons/ai'
 
-const FooterWrapper = styled('footer')(grid, layout, typography)
-const Icon = styled(AiTwotoneHeart)(color, space)
-
 const Footer = () => (
-  <Container>
-    <FooterWrapper display="grid" textAlign="center" fontSize={1}>
-      <span>
-        Made with <Icon size="16px" mb="-2px" color="primary" /> and{' '}
-        <a href="https://gatsbyjs.org" alt="Gatsby" rel="">
-          Gatsby
-        </a>
-      </span>
-      <span>Daniel Soares</span>
-    </FooterWrapper>
-  </Container>
+  <footer className="container my-16 grid text-xs text-center">
+    <span>
+      Made with <AiTwotoneHeart className="inline mb-1 text-sm text-red-500" />{' '}
+      and{' '}
+      <a
+        href="https://gatsbyjs.org"
+        alt="Gatsby"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Gatsby
+      </a>
+    </span>
+    <span>Daniel Soares</span>
+  </footer>
 )
 
 export default Footer
