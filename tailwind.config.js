f1e46983f58bde4b270cf56b058249700989747a
Daniel Soares <daniel.soares@me.com>
2020-10-13 11:11:14 -0300

    💄 Add container settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   },
   purge: ['./src/**/*.js', './src/**/*.jsx', './src/**/*.ts', './src/**/*.tsx'],
   theme: {
+    container: {
+      center: true,
+      padding: '2rem'
+    },
     extend: {}
   },
   variants: {},
