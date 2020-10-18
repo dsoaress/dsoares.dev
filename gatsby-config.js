4e5fa8a7068b5914caa1b6c987074da9f57564bc
Daniel Soares <daniel.soares@me.com>
2020-10-18 16:38:05 -0300

    ✨ Add sitemap

--- gatsby-config.js
+++ gatsby-config.js
@@ -10,6 +10,7 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
+    `gatsby-plugin-sitemap`,
     `gatsby-plugin-offline`,
     `gatsby-plugin-catch-links`,
     `gatsby-transformer-sharp`,
