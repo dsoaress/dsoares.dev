e42ba91805c7d261e2899207154f68b405114b5f
Daniel Soares <daniel.soares@me.com>
2020-10-19 15:32:52 -0300

    ✨ Add link to home page

--- src/components/header.js
+++ src/components/header.js
@@ -6,18 +6,29 @@ const Header = () => (
   <header className="border-t-8 border-highLight">
     <div className="container">
       <div className="flex justify-between items-center">
-        <Link to="/" alt="Home" className="font-black text-2xl border-none">
+        <Link to="/" alt="Home" className="border-none">
           <img src={logo} alt="Daniel Soares" width="160" height="23" />
         </Link>
 
-        <Link
-          to="/sobre-mim"
-          alt="Sobre mim"
-          activeClassName="active"
-          className="text-default hover:text-highLight border-none"
-        >
-          Sobre mim
-        </Link>
+        <div className="flex space-x-8">
+          <Link
+            to="/"
+            alt="Home"
+            activeClassName="active"
+            className="text-default hover:text-highLight border-none"
+          >
+            Home
+          </Link>
+
+          <Link
+            to="/sobre-mim/"
+            alt="Sobre mim"
+            activeClassName="active"
+            className="text-default hover:text-highLight border-none"
+          >
+            Sobre mim
+          </Link>
+        </div>
       </div>
     </div>
   </header>
