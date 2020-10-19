b8a37a53b78206c5234933da8d7d10abd7e456f4
Daniel Soares <daniel.soares@me.com>
2020-10-19 16:18:47 -0300

    ✨ Add profile to posts

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -3,10 +3,14 @@ import { graphql } from 'gatsby'
 import Layout from '@layout'
 import SEO from '@seo'
 import Post from '@components/post'
+import Profile from '@components/profile'
 
 const BlogPost = ({ data }) => {
-  const post = data.markdownRemark
-  const body = <div dangerouslySetInnerHTML={{ __html: post.html }} />
+  const post = data.post
+  const postBody = <div dangerouslySetInnerHTML={{ __html: post.html }} />
+  const profile = data.profile
+  const profileBody = <div dangerouslySetInnerHTML={{ __html: profile.html }} />
+  const socialLinks = data.socialLinks.siteMetadata.socialLinks
 
   return (
     <Layout>
@@ -15,18 +19,27 @@ const BlogPost = ({ data }) => {
         description={post.frontmatter.description}
       />
       <Post
-        body={body}
+        body={postBody}
         date={post.frontmatter.date}
         image={post.frontmatter.image}
         title={post.frontmatter.title}
       />
+      <div className="mx-4 md:mx-8">
+        <Profile
+          body={profileBody}
+          className="bg-muted rounded-xl shadow-md py-8"
+          image={profile.frontmatter.image}
+          socialLinks={socialLinks}
+          title={profile.frontmatter.title}
+        />
+      </div>
     </Layout>
   )
 }
 
 export const query = graphql`
-  query Post($slug: String!) {
-    markdownRemark(fields: { slug: { eq: $slug } }) {
+  query($slug: String!) {
+    post: markdownRemark(fields: { slug: { eq: $slug } }) {
       frontmatter {
         title
         description
@@ -41,6 +54,27 @@ export const query = graphql`
       }
       html
       timeToRead
+    }
+    profile: markdownRemark(frontmatter: { key: { eq: "profile" } }) {
+      frontmatter {
+        title
+        image {
+          childImageSharp {
+            fluid(maxWidth: 384, quality: 90) {
+              ...GatsbyImageSharpFluid_withWebp
+            }
+          }
+        }
+      }
+      html
+    }
+    socialLinks: site {
+      siteMetadata {
+        socialLinks {
+          label
+          url
+        }
+      }
     }
   }
 `
