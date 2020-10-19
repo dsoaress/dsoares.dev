1be8d694e5df7b7ae530ffb4e8c437fcb0a73034
Daniel Soares <daniel.soares@me.com>
2020-10-19 00:38:32 -0300

    🐛 Fix wp-admin preview

--- src/cms/cms.js
+++ src/cms/cms.js
@@ -2,10 +2,12 @@ import CMS from 'netlify-cms-app'
 import IndexPage from './templates/index-page'
 import AboutPage from './templates/about-page'
 import NotFoundPage from './templates/not-found-page'
+import WPAdminPage from './templates/wp-admin-page'
 import BlogPost from './templates/blog-post'
 import '@styles/tailwind.scss'
 
 CMS.registerPreviewTemplate('index', IndexPage)
 CMS.registerPreviewTemplate('about', AboutPage)
 CMS.registerPreviewTemplate('not-found', NotFoundPage)
+CMS.registerPreviewTemplate('wp-admin', WPAdminPage)
 CMS.registerPreviewTemplate('posts', BlogPost)
