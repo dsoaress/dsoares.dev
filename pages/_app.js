4ab755a23a9473ef29565081ffd9cbbb67df2f7f
Daniel Soares <daniel.soares@me.com>
2021-04-02 14:58:35 -0300

    feat: add fonts

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,7 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import theme from '@/theme'
+import '@fontsource/rubik-mono-one'
+import '@fontsource/rubik'
 
 export default function App({ Component, pageProps }) {
   return (
