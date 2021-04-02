b0c6d588992e1bf13dc3a85e29721ffd58af8d4d
Daniel Soares <daniel.soares@me.com>
2021-04-02 16:15:46 -0300

    refactor: remove next-pwa

--- next.config.js
+++ next.config.js
@@ -1,11 +1,6 @@
-const withPWA = require('next-pwa')
-
-module.exports = withPWA({
-  pwa: {
-    disable: process.env.NODE_ENV === 'development'
-  },
+module.exports = {
   i18n: {
     locales: ['en', 'es', 'pt'],
     defaultLocale: 'en'
   }
-})
+}
