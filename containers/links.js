4f935b8086faf092384e14f656d00f232da44b7e
Daniel Soares <daniel.soares@me.com>
2021-04-02 22:48:33 -0300

    refactor: imp theme

--- containers/links.js
+++ containers/links.js
@@ -7,15 +7,7 @@ export default function Links({ data }) {
       <Stack spacing={6}>
         {data.map((item, i) => (
           <Link href={item.url} key={i} isExternal>
-            <Button
-              bg="highlight"
-              color="white"
-              _hover={{ bg: 'hover' }}
-              py={8}
-              w="100%"
-            >
-              {item.label}
-            </Button>
+            <Button>{item.label}</Button>
           </Link>
         ))}
       </Stack>
---
+++ theme/components/container.js
@@ -0,0 +1,6 @@
+export default {
+  baseStyle: {
+    maxW: 'container.md',
+    padding: '0 32px'
+  }
+}
