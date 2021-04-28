661ac8ea1c7f3ee53f8ef1e56d47cc195fe7e6c1
Daniel Soares <daniel.soares@me.com>
2021-04-28 19:49:15 -0300

    feat: add service worker

--- next.config.js
+++ next.config.js
@@ -1,6 +1,14 @@
-module.exports = {
+const withPWA = require('next-pwa')
+
+const nextConfig = {
   i18n: {
     locales: ['en', 'es', 'pt'],
     defaultLocale: 'en'
+  },
+  pwa: {
+    dest: 'public',
+    disable: process.env.NODE_ENV === 'development'
   }
 }
+
+module.exports = withPWA(nextConfig)
