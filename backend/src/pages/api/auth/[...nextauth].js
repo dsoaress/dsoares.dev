b02b80164e7b43cc8f1badad5eddb4c7dcc8e6e3
Daniel Soares <daniel.soares@me.com>
2021-04-29 19:54:52 -0300

    fix: import

--- backend/src/pages/api/auth/[...nextauth].js
+++ backend/src/pages/api/auth/[...nextauth].js
@@ -1,11 +1,11 @@
 import NextAuth from 'next-auth'
-import { GitHub } from 'next-auth/providers'
+import Providers from 'next-auth/providers'
 
 const { GITHUB_ID, GITHUB_SECRET, MONGODB_URI } = process.env
 
 export default NextAuth({
   providers: [
-    GitHub({
+    Providers.GitHub({
       clientId: GITHUB_ID,
       clientSecret: GITHUB_SECRET
     })
