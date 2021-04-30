d75daa9a0aa15b0bc4d9c5db0eb281bbcb3c9542
Daniel Soares <daniel.soares@me.com>
2021-04-30 13:01:45 -0300

    fix: cors

--- backend/src/pages/api/index.js
+++ backend/src/pages/api/index.js
@@ -1,6 +1,13 @@
 import { connectToDatabase } from '@/lib/mongodb'
+import NextCors from 'nextjs-cors'
 
 export default async (req, res) => {
+  await NextCors(req, res, {
+    methods: ['GET', 'HEAD'],
+    origin: '*',
+    optionsSuccessStatus: 200
+  })
+
   const { db } = await connectToDatabase()
   const userEmail = process.env.USER_EMAIL
 
