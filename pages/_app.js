dbeec662ff12376836ecf85dee591f8dd3941402
Daniel Soares <daniel.soares@me.com>
2021-05-08 22:07:46 -0300

    feat: add plausible

--- pages/_app.js
+++ pages/_app.js
@@ -2,38 +2,23 @@ import { useEffect } from 'react'
 import { useRouter } from 'next/router'
 import { Provider } from 'next-auth/client'
 import { ChakraProvider } from '@chakra-ui/react'
-import * as Fathom from 'fathom-client'
+import PlausibleProvider from 'next-plausible'
 
 import theme from '@/theme'
 import '@fontsource/rubik-mono-one'
 import '@fontsource/rubik'
 
 export default function App({ Component, pageProps }) {
-  const { events } = useRouter()
-
-  useEffect(() => {
-    window.dataLayer = window.dataLayer || []
-
-    Fathom.load('GMSVDEIT', {
-      url: 'https://limpet.dsoares.me/script.js',
-      includedDomains: ['dsoares.me']
-    })
-
-    function onRouteChangeComplete() {
-      Fathom.trackPageview()
-    }
-
-    events.on('routeChangeComplete', onRouteChangeComplete)
-
-    return () => {
-      events.off('routeChangeComplete', onRouteChangeComplete)
-    }
-  }, [])
-
   return (
     <Provider session={pageProps.session}>
       <ChakraProvider theme={theme}>
-        <Component {...pageProps} />
+        <PlausibleProvider
+          domain="dsoares.me"
+          customDomain="https://stats.dsoares.me"
+          trackOutboundLinks={true}
+        >
+          <Component {...pageProps} />
+        </PlausibleProvider>
       </ChakraProvider>
     </Provider>
   )
