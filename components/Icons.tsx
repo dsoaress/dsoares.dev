ce6d5e05f609a2fa3bbed13fcbc9886d5c7c735e
Daniel Soares <daniel.soares@me.com>
2021-07-06 23:09:19 -0300

    feat: add dev.to

--- components/Icons.tsx
+++ components/Icons.tsx
@@ -1,15 +1,16 @@
 import {
   AiFillGithub as GitHub,
-  AiFillLinkedin as LinkedIn,
   AiOutlineInstagram as Instagram,
   AiOutlineMail as Email,
   AiOutlineWhatsApp as WhatsApp
 } from 'react-icons/ai'
+import { FaDev as Dev, FaLinkedinIn as LinkedIn } from 'react-icons/fa'
 
 const Icons = {
   Email,
   GitHub,
   LinkedIn,
+  Dev,
   Instagram,
   WhatsApp
 }
