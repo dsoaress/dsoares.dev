7613937b274fcd842d0b10ead2c993254ee31d33
Daniel Soares <daniel.soares@me.com>
2021-07-07 00:22:16 -0300

    chore: changes location of stories

--- .storybook/main.js
+++ .storybook/main.js
@@ -2,7 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 
 module.exports = {
-  stories: ['../components/**/*.stories.@(js|jsx|ts|tsx)'],
+  stories: ['../stories/**/*.stories.@(js|jsx|ts|tsx)'],
   addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
   core: {
     builder: 'webpack5'
