396b73d33033babd6a4c0afc2ecc220e7dc01b37
Daniel Soares <daniel.soares@me.com>
2021-07-15 23:48:00 -0300

    chore: remove spanish

--- locales.config.js
+++ locales.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   i18n: {
-    locales: ['en', 'es', 'pt'],
+    locales: ['en', 'pt'],
     defaultLocale: 'en'
   }
 }
--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,9 +1,7 @@
 import { en } from './en'
-import { es } from './es'
 import { pt } from './pt'
 
 export const i18n = {
   en,
-  es,
   pt
 }
