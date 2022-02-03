afac6a1987de0f1a0432ff4eb65411e18eb3b6dc
Daniel Soares <daniel.soares@me.com>
2022-02-03 00:11:46 -0300

    refactor: initialdata script

--- scripts/initialdata.ts
+++ scripts/initialdata.ts
@@ -1,6 +1,35 @@
-import 'dotenv/config'
-import 'isomorphic-fetch'
+import fs from 'fs'
 
-import { initialdata } from '@/services/initialdata'
+import { config } from '@/data/config'
 
-initialdata().catch(error => console.log(error))
+function initialdata() {
+  config.locales.locales.map(locale => {
+    const startUrl = locale === config.locales.defaultLocale ? '' : locale
+    const manifestDir = locale === config.locales.defaultLocale ? './public' : `./public/${locale}`
+
+    const manifest = {
+      name: config.profile.title,
+      short_name: config.profile.shortTitle,
+      start_url: `/${startUrl}`,
+      background_color: config.colors.neutral[900],
+      theme_color: config.colors.neutral[900],
+      display: 'fullscreen',
+      icons: config.favicons.map(favicon => {
+        return {
+          purpose: 'any maskable',
+          src: favicon.src,
+          sizes: `${favicon.size}x${favicon.size}`,
+          type: 'image/png'
+        }
+      })
+    }
+
+    if (!fs.existsSync(manifestDir)) {
+      fs.mkdirSync(manifestDir, { recursive: true })
+    }
+
+    fs.writeFileSync(`${manifestDir}/manifest.json`, JSON.stringify(manifest, null, 2))
+  })
+}
+
+initialdata()
