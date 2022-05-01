9e2417ff300bc3eae83835fa137b8f9db64d8d26
Daniel Soares <daniel.soares@me.com>
2022-04-30 23:28:01 -0300

    refactor: get current track

--- src/pages/api/get-current-track.ts
+++ src/pages/api/get-current-track.ts
@@ -2,24 +2,20 @@ import axios from 'axios'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
-  const { UPSTASH_URL, UPSTASH_TOKEN } = process.env
+  const { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = process.env
 
-  if (!UPSTASH_URL || !UPSTASH_TOKEN) {
+  if (!UPSTASH_REDIS_REST_URL || !UPSTASH_REDIS_REST_TOKEN) {
     return res.status(400).json({ error: 'UPSTASH_REST_URL or UPSTASH_REDIS_URL is not set' })
   }
 
   const upstashClient = axios.create({
-    baseURL: UPSTASH_URL,
+    baseURL: UPSTASH_REDIS_REST_URL,
     headers: {
-      Authorization: `Bearer ${UPSTASH_TOKEN}`
+      Authorization: `Bearer ${UPSTASH_REDIS_REST_TOKEN}`
     }
   })
 
-  const { data: isPlaying } = await upstashClient.get('isPlaying')
-  const { data: currentTrack } = await upstashClient.get('currentTrack')
+  const { data: currentTrack } = await upstashClient.get('current-track')
 
-  res.status(200).json({
-    isPlaying: isPlaying?.result === 'true',
-    track: JSON.parse(currentTrack?.result ?? '{}')
-  })
+  res.status(200).json(JSON.parse(currentTrack?.result ?? '{}'))
 }
