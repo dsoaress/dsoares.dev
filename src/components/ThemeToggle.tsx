ad63d6cb6b1cbaac3863b1d62630166bbab77e36
Daniel Soares <daniel.soares@me.com>
2022-05-02 16:04:28 -0300

    chore: imp theme toggle

--- src/components/ThemeToggle.tsx
+++ src/components/ThemeToggle.tsx
@@ -1,16 +1,9 @@
-import { useEffect, useState } from 'react'
 import { BiMoon, BiSun } from 'react-icons/bi'
 
 import { useTheme } from '@/hooks/useTheme'
 
 export function ThemeToggle() {
-  const [isMounted, setIsMounted] = useState(false)
   const { theme, setTheme } = useTheme()
-
-  useEffect(() => setIsMounted(true), [])
-
-  if (!isMounted) return null
-
   return (
     <li className="ml-6 text-neutral-50" aria-hidden>
       {theme === 'light' ? (
