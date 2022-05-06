789fc22da734071556699c2a03ced278a20ee91c
Daniel Soares <daniel.soares@me.com>
2022-05-06 14:08:37 -0300

    refactor: remove axios

--- src/hooks/useCurrentTrack.ts
+++ src/hooks/useCurrentTrack.ts
@@ -1,9 +1,9 @@
 import useSWR from 'swr'
 
-import { fetcher } from '@/services/fetcher'
 import type { CurrentTrack } from '@/types'
 
 export function useCurrentTrack() {
+  const fetcher = (url: string) => fetch(url).then(r => r.json())
   const { data } = useSWR<CurrentTrack>('/api/get-current-track', fetcher, {
     refreshInterval: 30000
   })
--- src/pages/api/get-current-track.ts
+++ src/pages/api/get-current-track.ts
@@ -1,21 +1,11 @@
-import axios from 'axios'
+import { Redis } from '@upstash/redis'
 import type { NextApiRequest, NextApiResponse } from 'next'
 
+import { CurrentTrack } from '@/types'
+
 export default async function handler(_req: NextApiRequest, res: NextApiResponse) {
-  const { UPSTASH_REDIS_REST_URL, UPSTASH_REDIS_REST_TOKEN } = process.env
+  const upstashClient = Redis.fromEnv()
+  const currentTrack = await upstashClient.get<CurrentTrack>('current-track')
 
-  if (!UPSTASH_REDIS_REST_URL || !UPSTASH_REDIS_REST_TOKEN) {
-    return res.status(400).json({ error: 'UPSTASH_REST_URL or UPSTASH_REDIS_URL is not set' })
-  }
-
-  const upstashClient = axios.create({
-    baseURL: UPSTASH_REDIS_REST_URL,
-    headers: {
-      Authorization: `Bearer ${UPSTASH_REDIS_REST_TOKEN}`
-    }
-  })
-
-  const { data: currentTrack } = await upstashClient.get('get/current-track')
-
-  res.status(200).json(JSON.parse(currentTrack?.result ?? '{}'))
+  res.status(200).json(currentTrack)
 }
