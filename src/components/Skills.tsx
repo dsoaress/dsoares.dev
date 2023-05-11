6c7f8bad52976f376af80d6916bad889b428b420
Daniel Soares <daniel.soares@me.com>
2023-05-10 21:56:16 -0300

    feat: update deps

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -1,4 +1,4 @@
-import { Icon, Tooltip } from '@/components'
+import { Icon, Tooltip, TooltipProvider } from '@/components'
 import { useData } from '@/hooks/useData'
 
 export function Skills() {
@@ -7,13 +7,15 @@ export function Skills() {
   return (
     <div className="mx-auto">
       <ul className="flex list-none flex-wrap justify-center gap-6 text-neutral-700 transition-colors duration-300 dark:text-neutral-200">
-        {c.skills?.map(label => (
-          <li key={label}>
-            <Tooltip content={label}>
-              <Icon variant={label} size={18} aria-label={label} />
-            </Tooltip>
-          </li>
-        ))}
+        <TooltipProvider>
+          {c.skills?.map(label => (
+            <li key={label}>
+              <Tooltip content={label}>
+                <Icon variant={label} size={18} aria-label={label} />
+              </Tooltip>
+            </li>
+          ))}
+        </TooltipProvider>
       </ul>
     </div>
   )
