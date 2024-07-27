c88c6c710adcba05ca1f0887fb521290e2e034ad
Daniel Soares <daniel.soares@me.com>
2024-07-27 12:55:23 -0300

    feat: add random color schema

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,6 +1,51 @@
 import type { Config } from 'tailwindcss'
-import colors from 'tailwindcss/colors'
+import {
+  amber,
+  blue,
+  cyan,
+  emerald,
+  fuchsia,
+  gray,
+  green,
+  indigo,
+  lime,
+  orange,
+  pink,
+  purple,
+  red,
+  rose,
+  sky,
+  slate,
+  stone,
+  teal,
+  violet,
+  zinc
+} from 'tailwindcss/colors'
 import { fontFamily } from 'tailwindcss/defaultTheme'
+
+const neutralPalette = [zinc, gray, slate, stone]
+const primaryPalette = [
+  rose,
+  pink,
+  fuchsia,
+  purple,
+  violet,
+  indigo,
+  cyan,
+  teal,
+  emerald,
+  lime,
+  amber,
+  orange,
+  red,
+  blue,
+  green,
+  sky
+]
+
+function randomColor(palette: typeof neutralPalette | typeof primaryPalette) {
+  return palette[Math.floor(Math.random() * palette.length)]
+}
 
 const config = {
   content: ['./src/**/*.{ts,tsx}'],
@@ -10,8 +55,8 @@ const config = {
         sans: ['var(--font-sans)', ...fontFamily.sans]
       },
       colors: {
-        neutral: colors.zinc,
-        primary: colors.sky
+        neutral: randomColor(neutralPalette),
+        primary: randomColor(primaryPalette)
       }
     }
   }
