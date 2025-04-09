9ba76b1e2c504cef852b25204f078f47ac63b405
Daniel Soares <daniel.soares@me.com>
2025-04-09 20:22:29 -0300

    feat: add umami

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,13 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [{ protocol: 'https', hostname: 'github.com' }]
-  }
+  },
+  rewrites: async () => [
+    {
+      source: '/script.js',
+      destination: 'https://umami.dsoares.dev/script.js'
+    }
+  ]
 }
 
 export default nextConfig
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,40 +1,48 @@
-import "@/styles/globals.css";
+import '@/styles/globals.css'
 
-import type { Metadata } from "next";
-import { Inter as FontSans } from "next/font/google";
+import type { Metadata } from 'next'
+import { Inter as FontSans } from 'next/font/google'
+import Script from 'next/script'
 
-import { cn } from "@/lib/cn";
+import { cn } from '@/lib/cn'
 
 const fontSans = FontSans({
-  subsets: ["latin"],
-  variable: "--font-sans",
-});
+  subsets: ['latin'],
+  variable: '--font-sans'
+})
 
 export const metadata: Metadata = {
-  title: "Daniel Soares",
-  description: "Hi! I&apos;m a full stack developer working with React, Next.js and Node.js",
-  robots: "follow, index",
+  title: 'Daniel Soares',
+  description: 'Hi! I&apos;m a full stack developer working with React, Next.js and Node.js',
+  robots: 'follow, index',
   icons: [
-    { rel: "icon", type: "image/svg+xml", url: "/favicon.svg" },
-    { rel: "icon", type: "image/png", url: "/favicon.png" },
-  ],
-};
+    { rel: 'icon', type: 'image/svg+xml', url: '/favicon.svg' },
+    { rel: 'icon', type: 'image/png', url: '/favicon.png' }
+  ]
+}
 
 type Props = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 export default function RootLayout({ children }: Readonly<Props>): React.JSX.Element {
   return (
     <html lang="en">
       <body
         className={cn(
-          "font-sans antialiased min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50",
+          'font-sans antialiased min-h-screen bg-zinc-900 px-4 py-12 text-zinc-50',
           fontSans.variable
         )}
       >
         {children}
+        <Script
+          src="/script.js"
+          data-host-url={process.env.NEXT_PUBLIC_UMAMI_HOST_URL}
+          data-website-id={process.env.NEXT_PUBLIC_UMAMI_ID}
+          strategy="beforeInteractive"
+          defer
+        />
       </body>
     </html>
-  );
+  )
 }
